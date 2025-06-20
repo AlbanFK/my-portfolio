0a3ebd3c57b574693a1fb7f40c78114ebe57a69a
AlbanFK <fokoalban3@gmail.com>
2025-06-20 18:16:24 -0400

    install tailwind css, nuxt google font and set default layout and page

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-screen w-screen p-3 font-[DM_Sans]">
+    <Navbar />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxtjs/google-fonts"],
+
+  googleFonts: {
+    families: {
+      "DM+Sans": true,
+    },
+  },
+});
